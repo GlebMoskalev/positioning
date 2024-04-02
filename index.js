8b05c7fddd931b7b4788a0697a110b217346f1eb
GlebMoskalev <gleb12122003@gmail.com>
2024-04-02 19:41:18 +0500

    add 1-4

--- index.js
+++ index.js
@@ -27,3 +27,27 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
+openModal.onclick = function() {
+    modal.style.display = "block";
+    move();
+}
+var i = 0;
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar1");
+        var width = 1;
+        var id = setInterval(frame, 30);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.clipPath = `inset(0 0 0 ${width}%)`;
+            }
+        }
+    }
+}
