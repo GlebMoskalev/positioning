d06ed54213318db81bf140e65793a60d44e7ad44
GlebMoskalev <gleb12122003@gmail.com>
2024-04-02 17:57:01 +0500

    add lightbox

--- index.js
+++ index.js
@@ -5,3 +5,25 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const modal = document.getElementById("modal");
+const openModal = document.getElementById("openModal");
+const closeModal1 = document.querySelector(".modal-content button");
+const closeModal2 = document.querySelector(".close");
+
+openModal.onclick = function() {
+    modal.style.display = "block";
+}
+
+closeModal1.onclick = function() {
+    modal.style.display = "none";
+}
+closeModal2.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
